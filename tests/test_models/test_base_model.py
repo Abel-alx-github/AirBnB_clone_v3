0fe65e284365a93ae41999ed98be61df5a7e41ac
abel-alx-github <abeltibeso323@gmail.com>
2024-03-29 05:40:23 -0700

    fork

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -83,14 +83,15 @@ class TestBaseModel(unittest.TestCase):
         and that upon creation have identical updated_at and created_at
         value."""
         tic = datetime.now()
+        toc = datetime.now()
         inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
+
+        self.assertTrue(tic <= toc <= inst1.created_at)
+        time.sleep(1)
         tic = datetime.now()
-        inst2 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
+        inst2 = BaseModel()
+        self.assertTrue(tic <= toc <= inst2.created_at)
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
