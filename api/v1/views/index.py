ec703224139869290f61fea59d34625220269208
abel-alx-github <abeltibeso323@gmail.com>
2024-03-30 08:34:10 -0700

    engine

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from models.amenity import Amenity
 from models.city import City
 from models.user import User
 from models.review import Review
-
+from models.place import Place
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -20,6 +20,7 @@ def stats():
     dic = {}
     dic['amenities'] = storage.count(Amenity)
     dic['cities'] = storage.count(City)
+    dic ['places'] = storage.count(Place)
     dic['reviews'] = storage.count(Review)
     dic['states'] = storage.count(State)
     dic['users'] = storage.count(User)
