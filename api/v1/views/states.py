88929a3f741973c984709fefd67025c83941d7d7
abel-alx-github <abeltibeso323@gmail.com>
2024-03-30 12:34:03 -0700

    state releated route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,16 +9,16 @@ from api.v1.views import app_views
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     states = storage.all('State')
-    state_list = [state.to_dict() for state in states.values()]
-    return jsonify(state_list)
+    if states:
+        state_list = [state.to_dict() for state in states.values()]
+        return jsonify(state_list)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
-    states = storage.all('State')
-    for state in states.values():
-        if state.id == state_id:
-            return jsonify(state.to_dict())
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
     abort(404)
 
 
@@ -27,7 +27,6 @@ def get_state_by_id(state_id):
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state:
-        print('deleting')
         storage.delete(state)
         storage.save()
         return make_response(jsonify({}), 200)
@@ -37,7 +36,7 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     if not request.get_json:
-        abort(404)
+        abort(404, 'Not a JSON')
     data = request.get_json()
     if 'name' in data:
         state = State(**data)
