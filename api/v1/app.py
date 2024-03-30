ad94790c6d9d00138f6bb4e7f648a4515e0347e0
abel-alx-github <abeltibeso323@gmail.com>
2024-03-30 04:52:46 -0700

    api not found respon

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.teardown_appcontext
 def close(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return jsonify({"error": "Not Found"}), 404
 
 
 if __name__ == '__main__':
