99889f838760401eef1f11f3b884390d73a2654e
abel-alx-github <abeltibeso323@gmail.com>
2024-04-01 08:05:06 -0700

    states api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" define method to handle states query"""
 from flask import jsonify, abort, make_response, request
 from models.state import State
 from models import storage
@@ -8,7 +8,10 @@ from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
+    """ retrive states"""
     states = storage.all(State).values()
+    if not state:
+        abort(404)
     if states:
         state_list = [state.to_dict() for state in states]
         return jsonify(state_list)
@@ -16,7 +19,7 @@ def get_all_states():
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
-    """ """
+    """get state by id """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -26,7 +29,7 @@ def get_state_by_id(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """ """
+    """ delete a state"""
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -37,7 +40,7 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """ """
+    """ post a state"""
     if not request.get_json():
         abort(400, 'Not a JSON')
     data = request.get_json()
@@ -52,7 +55,7 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
-    """ """
+    """ update a state"""
     if not request.get_json():
         abort(400, 'Not a JSON')
     data = request.get_json()
