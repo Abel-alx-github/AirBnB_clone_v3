5f533500eb59352b5f66913568e03b977959f65e
abel-alx-github <abeltibeso323@gmail.com>
2024-04-01 06:51:08 -0700

    reviews api

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,40 +9,41 @@ from flask import request, abort, jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('places/<place_id>/review', methods=['GET', 'POST'],
+@app_views.route('/places/<place_id>/reviews', methods=['GET', 'POST'],
                  strict_slashes=False)
 def get_place_review(place_id):
     """ get and put """
     if request.method == 'GET':
-         place = storage.get(Place, place_id)
-         if not place:
-             abort(404)
-         reviews = storage.all(Review).values()
-         if not review:
-             abort(404)
-         reviews_list = [review.to_dict() for review in reviews if review.place_id == place_id]
-         return jsonify(reviews_list), 200
+        place = storage.get(Place, place_id)
+        if not place:
+            abort(404)
+        reviews = storage.all(Review).values()
+        if not review:
+            abort(404)
+        reviews_list = [review.to_dict() for review in reviews
+                        if review.place_id == place_id]
+        return jsonify(reviews_list), 200
     elif request.method == 'POST':
-         if not request.get_json():
-             abort(400, 'Not a JSON')
-         data = request.get_json()
-         if not data:
-             abort(404)
-         if 'user_id' not in data:
-              abort(400, 'Missing user_id')
-         user = storage.get(User, data['user_id']) 
-         if not user:
-             abort(404)
-         if 'text' not in data:
-              abort(400, 'Missing text')
-         data['place_id'] = place_id
-         review = Review(**data)
-         review.save()
-         return jsonify(review.to_dict()), 201
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        data = request.get_json()
+        if not data:
+            abort(404)
+        if 'user_id' not in data:
+            abort(400, 'Missing user_id')
+        user = storage.get(User, data['user_id'])
+        if not user:
+            abort(404)
+        if 'text' not in data:
+            abort(400, 'Missing text')
+        data['place_id'] = place_id
+        review = Review(**data)
+        review.save()
+        return jsonify(review.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET', 'DELETE', 'PUT'],
-                  strict_slashes=False)
+                 strict_slashes=False)
 def get_review_by_id(review_id):
     """ get, delete, put"""
     if request.method == 'GET':
@@ -59,7 +60,7 @@ def get_review_by_id(review_id):
         return jsonify({}), 200
     elif request.method == 'PUT':
         if not request.get_json():
-             abort(400, 'Not a JSON')
+            abort(400, 'Not a JSON')
         data = request.get_json()
         if not data:
             abort(404)
