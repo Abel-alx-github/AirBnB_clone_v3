ec7bab45f28fb8e0e77f93947add013bc19524ce
abel-alx-github <abeltibeso323@gmail.com>
2024-04-01 07:53:49 -0700

    states api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,9 +8,9 @@ from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
-    states = storage.all('State')
+    states = storage.all(State).values()
     if states:
-        state_list = [state.to_dict() for state in states.values()]
+        state_list = [state.to_dict() for state in states]
         return jsonify(state_list)
 
 
@@ -39,21 +39,25 @@ def delete_state(state_id):
 def post_state():
     """ """
     if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
+        abort(400, 'Not a JSON')
     data = request.get_json()
-    if 'name' in data:
-        state = State(**data)
-        state.save()
-        return make_response(jsonify(state.to_dict()), 201)
-    return jsonify({'error': 'Missing name'}), 400
+    if not data:
+        abort(404)
+    if 'name' not in data:
+        abort(404)
+    state = State(**data)
+    state.save()
+    return jsonify(state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """ """
     if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
+        abort(400, 'Not a JSON')
     data = request.get_json()
+    if not data:
+        abort(404)
     state = storage.get(State, state_id)
     if not state:
         abort(404)
